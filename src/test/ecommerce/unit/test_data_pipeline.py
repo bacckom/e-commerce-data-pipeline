543e0136187c92027fd9d3058027be5ba82cd190
bacckom@sina.com <bacckom@sina.com>
2024-04-24 18:27:01 +0800

    modify unit test.

--- src/test/ecommerce/unit/test_data_pipeline.py
+++ src/test/ecommerce/unit/test_data_pipeline.py
@@ -1,10 +1,17 @@
+import pytest
+
 from ecommerce.env.environment import Environment
 from ecommerce.dataflow.pipeline.sales_data_pipeline import SalesDataPipeline
 from ecommerce.data_schema.customer_product_orders import CustomerProductOrderObject
 
 
-def test_sales_data_pipeline() -> None:
+@pytest.fixture
+def customer_product_orders() -> CustomerProductOrderObject:
     environment = Environment()
     pipeline = SalesDataPipeline(environment)
     result_df: CustomerProductOrderObject = pipeline.run()
-    assert not result_df.df.empty
+    return result_df
+
+
+def test_sales_data_pipeline(customer_product_orders: CustomerProductOrderObject) -> None:
+    assert not customer_product_orders.df.empty, "Data pipeline is not correct"
