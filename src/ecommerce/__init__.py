15f6610497b119a1261600020f4e4b0112747d23
bacckom@sina.com <bacckom@sina.com>
2024-04-25 10:36:09 +0800

    add pytest conf file for git action.

--- src/ecommerce/__init__.py
+++
@@ -1,7 +0,0 @@
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    # Read version from PKG metadata
-    __version__ = version("ecommerce")
-except PackageNotFoundError:
-    __version__ = "0.0.0"  # fall-back version
